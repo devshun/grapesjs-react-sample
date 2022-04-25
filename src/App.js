6b7a81ac008a6cb0cf0cd060d93da3a9d502403a
shunppe <70781900+shunppe@users.noreply.github.com>
2022-04-25 22:22:26 +0900

    add grapesjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+
+import grapesjs from 'grapesjs';
+
+import 'grapesjs/dist/css/grapes.min.css';
+import 'grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.css';
+import 'grapesjs/dist/grapes.min.js';
+import 'grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.js';
+
 
 function App() {
+  useEffect(() => {
+    grapesjs.init({
+      container: '#gjs',
+      plugins: ['gjs-preset-webpage'],
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id='gjs'></div>
+    </>
   );
 }
 
