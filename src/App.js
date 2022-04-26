f0c5e7b43558036acbe6a230ee335c314a57b2d3
shunppe <70781900+shunppe@users.noreply.github.com>
2022-04-26 12:51:27 +0900

    init editor

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import { useEffect } from 'react';
-
-import grapesjs from 'grapesjs';
-
-import 'grapesjs/dist/css/grapes.min.css';
-import 'grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.css';
-import 'grapesjs/dist/grapes.min.js';
-import 'grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.js';
-
+import { initEditor } from './config';
 
 function App() {
   useEffect(() => {
-    grapesjs.init({
-      container: '#gjs',
-      plugins: ['gjs-preset-webpage'],
-    });
+    initEditor();
   }, []);
+
   return (
     <>
       <div id='gjs'></div>
